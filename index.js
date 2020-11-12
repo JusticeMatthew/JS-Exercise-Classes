ba24447e5c5ac58c0f68b6b83c371edfdc7fd570
Matthew Justice <matthewajustice@gmail.com>
2020-11-12 17:12:36 -0500

    2/6

--- index.js
+++ index.js
@@ -74,7 +74,26 @@ class Person {
 */
 
 class Car {
-
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons) {
+    this.tank += gallons;
+  }
+  drive(distance) {
+    const iCanGo = this.tank * this.milesPerGallon
+    if (this.tank > 0 && iCanGo >= distance) {
+      this.odometer = distance;
+      this.tank = (this.tank * this.milesPerGallon - distance) / this.milesPerGallon;
+    } else {
+      this.odometer = iCanGo;
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer} miles!`;
+    }
+  }  
 }
 
 /*
